b5c1e0ab9047856fe628f4562601261f03efdc57
aman-4321 <amanclash4321@gmail.com>
2025-01-22 17:31:09 +0530

    upated axios instance url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_API_URL
+      ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1`
       : "http://localhost:8081/api/v1",
   withCredentials: true,
 });
